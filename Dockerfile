1576013cf1888e0dc712c46835f859a0d3af1f4c
Brady Todhunter <bradyt@anchore.com>
2019-01-30 01:30:58 -0800

    functional postgres & registry
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
-FROM anchore/anchore-engine:v0.3.2
-
-ENV PG_MAJOR 9.6
-ENV PGDATA /var/lib/postgresql/data
+FROM anchore/anchore-engine:dev
 
 RUN apt-get update; \
     apt-get upgrade; \
-    apt-get install -y ca-certificates
+    apt-get install -y ca-certificates wget gosu
 
 # explicitly set user/group IDs
 RUN set -ex; \
@@ -15,39 +12,48 @@ RUN set -ex; \
 # also create the postgres user's home directory with appropriate permissions
 # see https://github.com/docker-library/postgres/issues/274
     mkdir -p /var/lib/postgresql; \
-    chown -R postgres:postgres /var/lib/postgresql
+    chown -R postgres:postgres /var/lib/postgresql; \
+    mkdir /docker-entrypoint-initdb.d; \
+    rm -f /config/config.yaml
 
-# grab gosu for easy step-down from root
-ENV GOSU_VERSION 1.11
-RUN set -x \
-	&& apt-get update && apt-get install -y --no-install-recommends ca-certificates wget && rm -rf /var/lib/apt/lists/* \
-	&& wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" \
-	&& wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" \
-	&& export GNUPGHOME="$(mktemp -d)" \
-	&& gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
-	&& gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
-	&& { command -v gpgconf > /dev/null && gpgconf --kill all || :; } \
-	&& rm -rf "$GNUPGHOME" /usr/local/bin/gosu.asc \
-	&& chmod +x /usr/local/bin/gosu \
-	&& gosu nobody true \
-    && apt-get purge -y --auto-remove ca-certificates wget
-
-RUN mkdir /docker-entrypoint-initdb.d
+ENV PG_MAJOR 9.6
+ENV PGDATA /var/lib/postgresql/data
 
 RUN set -eux; \
-    echo 'deb http://apt.postgresql.org/pub/repos/apt/ 18.04-pgdg main' > /etc/apt/sources.list.d/pgdg.list; \
+    export DEBIAN_FRONTEND=noninteractive; \
+    export DEBCONF_NONINTERACTIVE_SEEN=true; \
+    echo 'tzdata tzdata/Areas select Etc' | debconf-set-selections; \
+    echo 'tzdata tzdata/Zones/Etc select UTC' | debconf-set-selections; \
+    echo 'deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main' > /etc/apt/sources.list.d/pgdg.list; \
     curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -; \
     apt-get update; \
-    apt-get install -y postgresql-common; \
+    apt-get install -y --no-install-recommends postgresql-common; \
     sed -ri 's/#(create_main_cluster) .*$/\1 = false/' /etc/postgresql-common/createcluster.conf; \
     apt-get install -y "postgresql-${PG_MAJOR}"; \
-    apt-get purge -y --auto-remove;
+    rm -rf /var/lib/apt/lists/*; \
+    apt-get purge -y --auto-remove
 
 RUN mkdir -p /var/run/postgresql && chown -R postgres:postgres /var/run/postgresql && chmod 2775 /var/run/postgresql
 RUN mkdir -p "$PGDATA" && chown -R postgres:postgres "$PGDATA" && chmod 700 "$PGDATA" 
 
-VOLUME /var/lib/postgresql/data
+ENV REGISTRY_VERSION 2.7
+
+RUN set -eux; \
+    mkdir -p /etc/docker/registry; \
+    wget -O /usr/local/bin/registry https://github.com/docker/distribution-library-image/raw/release/${REGISTRY_VERSION}/amd64/registry; \
+    chmod +x /usr/local/bin/registry; \
+    wget -O /etc/docker/registry/config.yml https://raw.githubusercontent.com/docker/distribution-library-image/release/${REGISTRY_VERSION}/amd64/config-example.yml; \
+    apt-get purge -y ca-certificates wget
+
+COPY anchore-bootstrap.sql.gz /docker-entrypoint-initdb.d/
+COPY conf/stateless_ci_config.yaml /config/config.yaml
+COPY scripts/anchore_ci_tools.py /usr/local/bin/
 COPY docker-entrypoint.sh /usr/local/bin/
 
+ENV ANCHORE_CLI_URL "http://anchore-ci:8228/v1"
+ENV ANCHORE_HOST_ID anchore-ci
+ENV ANCHORE_ENDPOINT_HOSTNAME anchore-ci
+
+VOLUME ["/var/lib/registry"]
+EXPOSE 5432 5000
 ENTRYPOINT ["docker-entrypoint.sh"]
-EXPOSE 5432
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,58 +1,121 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-export POSTGRES_USER=postgres
-export POSTGRES_DB=postgres
+export POSTGRES_USER="${POSTGRES_USER:-postgres}"
+export POSTGRES_DB="${POSTGRES_DB:-postgres}"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-mysecretpassword}"
 
-eval 'initdb --username=${POSTGRES_USER} --pwfile=<(echo "$POSTGRES_PASSWORD")'
+export ANCHORE_DB_PASSWORD="$POSTGRES_PASSWORD"
+export ANCHORE_DB_USER="$POSTGRES_USER"
+export ANCHORE_DB_NAME="$POSTGRES_DB"
+export ANCHORE_DB_HOST='anchore-ci'
 
-if [ -n "$POSTGRES_PASSWORD" ]; then
-    authMethod=md5
+export PATH=$PATH:/usr/lib/postgresql/9.6/bin/
+echo "127.0.0.1 anchore-ci" >> /etc/hosts
+
+init_db () {
+    # look specifically for PG_VERSION, as it is expected in the DB dir. 
+    # Prevents DB initialization if data exists in $PGDATA
+    if [ ! -s "$PGDATA/PG_VERSION" ]; then
+
+        gosu postgres bash -c 'initdb --username=postgres --pwfile=<(echo "$POSTGRES_PASSWORD")'
+
+        if [ -n "$POSTGRES_PASSWORD" ]; then
+            authMethod=md5
+        else
+            authMethod=trust
+        fi
+
+        {
+            echo
+            echo "host all all all $authMethod"
+        } >> "$PGDATA/pg_hba.conf"
+
+        # internal start of server in order to allow set-up using psql-client
+        # does not listen on external TCP/IP and waits until start finishes
+        PGUSER="${PGUSER:-$POSTGRES_USER}" \
+        gosu postgres bash -c 'pg_ctl -D "$PGDATA" \
+            -o "-c listen_addresses=''" \
+            -w start'
+
+        export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+        psql=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password )
+        psql+=( --dbname "$POSTGRES_DB" )
+        export psql
+
+        echo
+        gosu postgres bash -c '\
+        export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+        psql=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password )
+        psql+=( --dbname "$POSTGRES_DB" )
+        export psql
+        for f in /docker-entrypoint-initdb.d/*; do
+            case "$f" in
+                *.sh)
+                    # https://github.com/docker-library/postgres/issues/450#issuecomment-393167936
+                    # https://github.com/docker-library/postgres/pull/452
+                    if [ -x "$f" ]; then
+                        echo "$0: running $f"
+                        "$f"
+                    else
+                        echo "$0: sourcing $f"
+                        . "$f"
+                    fi
+                    ;;
+                *.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
+                *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+                *)        echo "$0: ignoring $f" ;;
+            esac
+            echo
+        done'
+
+        PGUSER="${PGUSER:-$POSTGRES_USER}" \
+        gosu postgres bash -c 'pg_ctl -D "$PGDATA" -m fast -w stop'
+
+        unset PGPASSWORD
+
+        echo
+        echo 'PostgreSQL init process complete; ready for start up.'
+        echo
+    fi
+}
+
+start_services () {
+    touch /var/log/postgres.log && chown postgres:postgres /var/log/postgres.log
+    echo "starting postgresql."
+    nohup gosu postgres bash -c 'postgres &> /var/log/postgres.log &'
+    echo "starting docker registry."
+    nohup registry serve /etc/docker/registry/config.yml &> /var/log/registry.log &
+    echo "starting anchore engine."
+    nohup anchore-manager service start --all &> /var/log/anchore.log &
+}
+
+anchore_analysis () {
+    echo "$1"
+    image_name="${1%.*}"
+    echo "$image_name"
+    skopeo copy --dest-tls-verify=false docker-archive:/anchore-engine/${1} docker://anchore-ci:5000/${image_name}:latest
+    anchore-cli image add anchore-ci:5000/${image_name}:latest
+    anchore-cli image wait anchore-ci:5000/${image_name}:latest
+    anchore-cli --json evaluate check anchore-ci:5000/${image_name}:latest
+    anchore-cli --json image vuln anchore-ci:5000/${image_name}:latest all
+    anchore-cli --json image content anchore-ci:5000/${image_name}:latest os
+}
+
+if [ ! $# -eq 0 ]; then
+    if [ $1 = 'debug' ]; then
+        init_db
+        start_services
+        exec "${@:2}"
+    elif [ $1 = 'preload' ]; then
+        init_db
+        exec "${@:2}"
+    else
+        exec "$@"
+    fi
 else
-    authMethod=trust
+    init_db
+    start_services
+    anchore-cli system wait
+    export -f anchore_analysis
+    find /anchore-engine/ -type f -name "*.tar" -exec bash -c 'anchore_analysis `basename "$0"`' {} \;
 fi
-
-{
-    echo
-    echo "host all all all $authMethod"
-} >> "$PGDATA/pg_hba.conf"
-
-# internal start of server in order to allow set-up using psql-client
-# does not listen on external TCP/IP and waits until start finishes
-PGUSER="${PGUSER:-$POSTGRES_USER}" \
-pg_ctl -D "$PGDATA" \
-    -o "-c listen_addresses=''" \
-    -w start
-
-export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-psql=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password )
-psql+=( --dbname "$POSTGRES_DB" )
-
-echo
-for f in /docker-entrypoint-initdb.d/*; do
-    case "$f" in
-        *.sh)
-            # https://github.com/docker-library/postgres/issues/450#issuecomment-393167936
-            # https://github.com/docker-library/postgres/pull/452
-            if [ -x "$f" ]; then
-                echo "$0: running $f"
-                "$f"
-            else
-                echo "$0: sourcing $f"
-                . "$f"
-            fi
-            ;;
-        *.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
-        *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
-        *)        echo "$0: ignoring $f" ;;
-    esac
-    echo
-done
-
-PGUSER="${PGUSER:-$POSTGRES_USER}" \
-pg_ctl -D "$PGDATA" -m fast -w stop
-
-unset PGPASSWORD
-
-echo
-echo 'PostgreSQL init process complete; ready for start up.'
-echo
