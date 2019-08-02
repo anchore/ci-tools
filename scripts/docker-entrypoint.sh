9893fd9e55682fd2b8b5b5c14f4764146bc5e898
Brady Todhunter <bradyt@anchore.com>
2019-08-02 15:28:18 -0700

    fix volume mounting file parsing logic. Add debug/verbose option
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -eo pipefail
 
+if [[ "${VERBOSE}" ]]; then
+    set -x
+fi
+
 main() {
     # use 'debug' as the first input param for script. This starts all services, then execs all proceeding inputs
     if [[ "$1" = 'debug' ]]; then
