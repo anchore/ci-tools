ee847b7278cf90919a8e6232f1a0c811a14b0161
Brady Todhunter <bradyt@anchore.com>
2019-08-02 17:49:36 -0700

    dont retag built image
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -215,10 +215,9 @@ build_image() {
     DOCKER_RUN_IDS+=("${db_preload_id:0:6}")
 
     docker build --build-arg "ANCHORE_VERSION=${anchore_version}" -t "${IMAGE_REPO}:dev" .
-    docker tag "${IMAGE_REPO}:dev" "${IMAGE_REPO}:dev-${anchore_version}"
     if [[ ! "$1" == 'dev' ]]; then
         local docker_name="${RANDOM:-temp}-db-preload"
-        docker run -it --name "$docker_name" "${IMAGE_REPO}:dev-${anchore_version}" debug /bin/bash -c "anchore-cli system wait --feedsready 'vulnerabilities,nvd' && anchore-cli system status && anchore-cli system feeds list"
+        docker run -it --name "$docker_name" "${IMAGE_REPO}:dev" debug /bin/bash -c "anchore-cli system wait --feedsready 'vulnerabilities,nvd' && anchore-cli system status && anchore-cli system feeds list"
         local docker_id=$(docker inspect $docker_name | jq '.[].Id')
         docker kill "$docker_id" && docker rm "$docker_id"
         DOCKER_RUN_IDS+=("${docker_id:0:6}")
@@ -261,7 +260,7 @@ test_bulk_image_volume() {
 
 test_inline_image() {
     local anchore_version="$1"
-    export ANCHORE_CI_IMAGE="${IMAGE_REPO}:dev-${anchore_version}"
+    export ANCHORE_CI_IMAGE="${IMAGE_REPO}:dev"
     cat "${WORKING_DIRECTORY}/scripts/inline_scan" | bash -s -- -V -p  alpine:latest
 }
 
