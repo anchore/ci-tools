0c98b729cb477df2396a4fd57f9e963aa151a030
Brady Todhunter <bradyt@anchore.com>
2019-08-29 10:33:36 -0700

    increase timeout on test anchore-engine scan to 1500
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -270,7 +270,7 @@ test_inline_script() {
     docker pull docker:stable-git
     curl -s "$INLINE_URL" | bash -s -- -d ".circleci/Dockerfile" docker:stable-git
     # test script with policy bundle
-    curl -s "$INLINE_URL" | bash -s -- -p  -b ".circleci/.anchore/policy_bundle.json" "anchore/anchore-engine:latest"
+    curl -s "$INLINE_URL" | bash -s -- -p  -t 1500 -b ".circleci/.anchore/policy_bundle.json" "anchore/anchore-engine:latest"
     # test script with policy bundle & dockerfile
     pushd "${WORKING_DIRECTORY}/.circleci/node_critical_pass/"
     docker build -t example.com:5000/ci-test_1/node_critical-pass:latest .
