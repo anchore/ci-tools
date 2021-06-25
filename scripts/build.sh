80ce0252222669598a7e3a8acdf3b0110e4d5888
Robert Prince <robert.prince@anchore.com>
2021-06-25 14:59:01 -0700

    Fix keyserver; bump scan timeout; don't build older versions
    Signed-off-by: Robert Prince <robert.prince@anchore.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,7 +35,7 @@ EOF
 ##############################################
 
 # Specify what versions to build & what version should get 'latest' tag
-export BUILD_VERSIONS=('v0.9.3' 'v0.9.2' 'v0.9.1')
+export BUILD_VERSIONS=('v0.9.3')
 export LATEST_VERSION='v0.9.3'
 
 # PROJECT_VARS are custom vars that are modified between projects
